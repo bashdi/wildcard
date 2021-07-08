7d19f19bee44d878a4c77f2505c19fe576ace5f5
Sebastian Renzer <sebastian.renzer@buttinette.de>
2021-07-08 14:23:33 +0200

    commons: Added Time.toSeconds

--- wildcard-commons/src/main/java/com/github/bashdi/wildcard/commons/utilities/Time.java
+++ wildcard-commons/src/main/java/com/github/bashdi/wildcard/commons/utilities/Time.java
@@ -9,21 +9,45 @@ public class Time {
         return addToCurrentTime(0,0, seconds);
     }
 
+
+
     public static LocalDateTime addToCurrentTime(int minutes, int seconds) {
         return addToCurrentTime(0,minutes, seconds);
     }
+
+
 
     public static LocalDateTime addToCurrentTime(int hours, int minutes, int seconds) {
         return  getCurrentTime().plusHours(hours).plusMinutes(minutes).plusSeconds(seconds);
     }
 
+
+
     public static LocalDateTime getCurrentTime() {
         return LocalDateTime.now();
     }
 
+
+
     public static String localDateTimeToString(LocalDateTime localDateTime) {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm");
         return dateTimeFormatter.format(localDateTime);
+    }
+
+
+
+    public static int toSeconds(int hours, int minutes) {
+        return toSeconds(0, hours, minutes);
+    }
+
+
+
+    public static int toSeconds(int days, int hours, int minutes) {
+        int seconds = 0;
+        hours += days * 24;
+        minutes += hours * 60;
+        seconds = minutes * 60;
+        return seconds;
     }
 
 }
